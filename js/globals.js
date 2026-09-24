c92011b6143a2b33579ad621429a93d007864b98
ASKXAXIS <ryo06040123@gmail.com>
2026-09-24 09:59:17 +0900

    ファイル修正

--- js/globals.js
+++ js/globals.js
@@ -3,6 +3,26 @@
  * グローバル変数・定数・DBデータ・初期化・保存関数・ユーティリティ
  */
 
+// デッキ作成
+var deckMode = "owned";
+// DS ->デッキセット：選択中のデッキの情報
+var DS = {
+  bladeLine: null, // 'bx','ux','cx'各ラインの種別
+  cxPat: 3, // 3 or 4　cxラインの場合3ピース型か4ピース型のどっちか
+  blade: null, // BX/UXラインはブレードの名前そのまま
+  lock: null,
+  main: null,
+  assist: null, // CXラインの3ピースの各ピースの名前
+  lock4: null,
+  metal: null,
+  over: null,
+  assist4: null, // CXラインの４ピースの各ピースの名前
+  ratchet: null, //ラチェット名
+  bit: null, //ビット名
+  combo: null, //ラチェット+ビット1体型
+};
+var userEditedName = false;
+
 /**
  * データの初期化(起動時にローカルストレージで保存されているデータを読み込む)
  */
